c4cd43cc66e97ac8c4ae3c3afd2768c6643f080c
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-31 15:55:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@ require 'pry'
 def run_guessing_game
   user_input = gets.chomp
   rand_num = rand(6) + 1
-  if user_input == rand_num
-    puts "/You guessed the correct number/"
-  elsif user_input != rand_num
-  puts "/Sorry! The computer guessed #{rand_num}/"
+  if user_input != rand_num
+    puts"/Sorry! The computer guessed #{rand_num}/"
+  elsif user_input == rand_num
+  puts "You guessed the correct number!"
   end
 end
