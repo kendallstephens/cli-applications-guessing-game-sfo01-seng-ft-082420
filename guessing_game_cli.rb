f597b67b21c03255189b128e6998e8f893a3366a
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-31 15:47:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,6 +5,6 @@ def run_guessing_game
   if user_input == rand_num
     puts "You guessed the correct number"
   elsif user_input != rand_num
-  puts "Sorry! The computer guessed #{rand_num}"
+  puts "/Sorry! The computer guessed #{rand_num}/"
   end
 end
