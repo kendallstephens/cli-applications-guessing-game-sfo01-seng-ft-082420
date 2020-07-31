87389356eef22405751217be87df245a8e12992e
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-31 16:32:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def run_guessing_game
-  user_input = gets.chomp.to_i
+  user_input = gets.chomp
   rand_num = rand(6) + 1
   if user_input != rand_num
     puts"/Sorry! The computer guessed #{rand_num}/"
