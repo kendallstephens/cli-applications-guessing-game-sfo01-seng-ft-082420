b45b910bbe3fb299767405ffc5ec62cdef49fe42
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-31 15:41:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,6 +2,6 @@ require 'pry'
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_input = gets.chomp
-  comp_num = rand(6) + 1
-  if 
+  rand_num = rand(6) + 1
+  if user_input == 
 end
